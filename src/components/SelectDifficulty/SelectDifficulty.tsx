fe790154a4c26bc20d440048f2accd2a0435eaad
Juan Gallego <jpgarias09@gmail.com>
2024-09-01 01:38:33 -0500

    feat: :sparkles: Introduced selection of difficulty

--- src/components/SelectDifficulty/SelectDifficulty.tsx
+++ src/components/SelectDifficulty/SelectDifficulty.tsx
@@ -1,34 +1,58 @@
-import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { apiUrl } from "../../contants";
+import { useSudokuContext } from "../Sudoku/useSudokuContext";
+
+import { useState } from "react";
+import style from "./SelectDifficulty.module.css";
 
 type IResponse = {
   option: string;
   value: string;
 };
+
+type IResponsee = {
+  solved: number[][];
+  unsolved: number[][];
+};
+
 const getDifficulties = async (): Promise<IResponse[]> => {
   const res = await fetch(apiUrl + "difficulties");
   return await res.json();
 };
+
 export default function SelectDifficulty() {
-  const [difficultySelected, setDifficultySelected] = useState<
-    string | undefined
-  >();
+  const queryClient = useQueryClient();
+  const { setInitialBoard, resetSudoku } = useSudokuContext();
+  const [difficultySelected, setDifficultySelected] = useState<string>();
+
   const { isPending, error, data } = useQuery<IResponse[]>({
     queryKey: ["sudokuDifficulty"],
     queryFn: getDifficulties,
   });
 
-  useEffect(() => {}, [difficultySelected]);
+  const handleClick = async (diff: string) => {
+    const data = await queryClient.fetchQuery({
+      queryKey: ["sudokuData"],
+      queryFn: async (): Promise<IResponsee> => {
+        const res = await fetch(apiUrl + `sudoku/${diff}`);
+        return await res.json();
+      },
+    });
+    setDifficultySelected(diff);
+    setInitialBoard(data.unsolved);
+    resetSudoku();
+  };
 
   if (isPending) return "Loading...";
   if (error) return "An error has occurred: " + error.message;
   return (
-    <div style={{ marginBottom: "10px" }}>
+    <div className={style.container}>
       {data.map((e) => (
         <button
-          style={{ margin: "" }}
-          onClick={() => setDifficultySelected(e.value)}
+          className={`${style.text} ${
+            e.value == difficultySelected && style.selected
+          }`}
+          onClick={() => handleClick(e.value)}
         >
           {e.option}
         </button>
